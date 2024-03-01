5afd50b631dbd3d2c1f65f6a4999ebcc79bc4c80
Naman-Parlecha <namanparlecha@gmail.com>
2024-03-02 01:26:34 +0530

    Nav Ui (╥_╥)

--- src/pages/TypingTest.jsx
+++ src/pages/TypingTest.jsx
@@ -2,8 +2,8 @@ import { signOut } from "firebase/auth";
 import React from "react";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-// import NavBar from "../components/NavBar";
-import Example from "../components/NavBar";
+import NavBar from "../components/NavBar";
+import "./Typingtest.scss";
 
 export default function TypingTest() {
   const navigate = useNavigate();
@@ -13,5 +13,9 @@ export default function TypingTest() {
     localStorage.removeItem("user");
     navigate("/login");
   }
-  return <Example />;
+  return (
+    <div className="typeTestPageCon">
+      <NavBar />
+    </div>
+  );
 }
